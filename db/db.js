cfde7f1175c4254022cd8a4b601761a106e79bd0
adidam23 <aditi.dam@gmail.com>
2022-02-23 11:54:11 -0500

    change try

--- db/db.js
+++ db/db.js
@@ -9,7 +9,7 @@ const dbConfig = {
   port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD, 
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_DATABASE || web_t2_adidam23,
   connectTimeout: parseInt(process.env.DB_CONNECT_TIMEOUT) || 10000
 }
 
